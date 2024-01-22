8315a78d70d07fc51a0041dbb91d1b90f40a5f9f
David Billdal <Davidbilldal@gmail.com>
2024-01-22 10:01:47 +0100

    feat: add quarkus packages for postgres

--- backend/feature-toggle-service/build.gradle.kts
+++ backend/feature-toggle-service/build.gradle.kts
@@ -20,6 +20,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-resteasy")
+    implementation("io.quarkus:quarkus-security-jpa")
+    implementation("io.quarkus:quarkus-hibernate-orm-panache-kotlin")
+    implementation("io.quarkus:quarkus-jdbc-postgresql")
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
 }
